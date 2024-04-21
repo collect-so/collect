ea8721fcec6749fe3e6ef22d412e7baab5df99b5
Artemy Vereshinsky <artemiy.vereshchinskiy@gmail.com>
2024-04-21 22:14:45 +0700

    fix(sdk, types): fix issue with nodejs http layer data normalization (#46)
    Co-authored-by: Artemiy Vereshchinskiy <artemiy.vereshchinsky@gmail.com>

--- packages/javascript-sdk/src/sdk/types.ts
+++ packages/javascript-sdk/src/sdk/types.ts
@@ -1,23 +1,26 @@
 import type { HttpClientInterface } from '../network/HttpClient'
 import type { Validator } from '../validators/types'
 
+type ApiConnectionConfig =
+  | {
+      host: string
+      port: number
+      protocol: string
+    }
+  | {
+      url: string
+    }
+
 export type CollectState = {
   debug: boolean
   timeout: number
   token?: string
-}
+} & Partial<ApiConnectionConfig>
 
 type CommonUserProvidedConfig = {
   httpClient?: HttpClientInterface
   timeout?: number
   validator?: Validator
-}
-export type UserProvidedConfig =
-  | (CommonUserProvidedConfig & {
-      host: string
-      port: number
-      protocol: string
-    })
-  | (CommonUserProvidedConfig & {
-      url: string
-    })
+} & ApiConnectionConfig
+
+export type UserProvidedConfig = CommonUserProvidedConfig
